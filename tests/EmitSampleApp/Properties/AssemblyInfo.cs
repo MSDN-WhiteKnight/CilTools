f63e51df98e3aa613cee7884679629e8d4578ca3
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2020-03-08 16:39:16 +0500

    Add sample app for dynamic methods testing

---
+++ tests/EmitSampleApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿/* CilBytecodeParser library 
+ * Copyright (c) 2019,  MSDN.WhiteKnight (https://github.com/MSDN-WhiteKnight) 
+ * License: BSD 2.0 */
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmitSampleApp")]
+[assembly: AssemblyDescription("CIL Bytecode Parser sample app")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EmitSampleApp")]
+[assembly: AssemblyCopyright("Copyright © MSDN.WhiteKnight 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a692fe9f-c078-45fe-b9ba-0733f857bb2c")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
