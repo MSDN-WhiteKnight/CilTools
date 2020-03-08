30ad28fefdc4d6a32ea7f74e799eb0e34b4b3073
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2020-03-08 16:38:31 +0500

    Move tests into separate dir

---
+++ tests/CilBytecodeParser.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CilBytecodeParser.Tests")]
+[assembly: AssemblyDescription("CilBytecode parser unit tests")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CilBytecodeParser.Tests")]
+[assembly: AssemblyCopyright("Copyright © MSDN.WhiteKnight 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5e050489-94ed-4cee-9ba2-8b9dd392441f")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
