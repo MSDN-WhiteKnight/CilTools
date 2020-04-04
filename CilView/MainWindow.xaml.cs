66c444c3cacd7cccdc2fac61f282b1beb4f16349
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2020-04-04 15:57:26 +0500

    Add CilView project

---
+++ CilView/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿/* CIL Tools 
+ * Copyright (c) 2020, MSDN.WhiteKnight (https://github.com/MSDN-WhiteKnight) 
+ * License: BSD 2.0 */
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CilTools.BytecodeAnalysis;
+using CilTools.Runtime;
+
+namespace CilView
+{
+    /// <summary>
+    /// CIL View main window codebehind
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static void DumpMethods(int pid, TextWriter target)
+        {
+            Process process = Process.GetProcessById(pid);
+            using (process)
+            {
+                DumpMethods(process,target);
+            }
+        }
+
+        public static void DumpMethods(string processname, TextWriter target)
+        {
+            Process[] processes = Process.GetProcessesByName(processname);
+            if (processes.Length == 0)
+            {
+                Console.WriteLine("Process not found");
+                return;
+            }
+
+            Process process = processes[0];
+
+            using (process)
+            {
+                DumpMethods(process, target);
+            }
+        }
+
+        public static void DumpMethods(Process process, TextWriter target)
+        {
+            //prints bytecode of methods in specified managed process
+
+            string module = "";
+            module = System.IO.Path.GetFileName(process.MainModule.FileName);
+
+            target.WriteLine("Process ID: {0}; Process name: {1}", process.Id, module);
+            target.WriteLine();
+
+            foreach (MethodBase m in ClrAssemblyReader.EnumerateModuleMethods(process))
+            {
+                target.WriteLine(" Method: " + m.DeclaringType.Name + "." + m.Name);
+
+                CilGraph gr = CilAnalysis.GetGraph(m);
+                target.WriteLine(gr.ToString());
+
+                target.WriteLine();                
+            }
+
+            DynamicMethodsAssembly dynass = ClrAssemblyReader.GetDynamicMethods(process);
+
+            using (dynass)
+            {
+                foreach (MethodBase m in dynass.EnumerateMethods())
+                {
+                    target.WriteLine("Method: " + m.DeclaringType.Name + "." + m.Name);
+
+                    CilGraph gr = CilAnalysis.GetGraph(m);
+                    target.WriteLine(gr.ToString());
+
+                    target.WriteLine();                    
+                }
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void bShow_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder sb = new StringBuilder(2000);
+            StringWriter wr = new StringWriter(sb);
+
+            using (wr)
+            {
+                DumpMethods(tbProcessName.Text, wr);
+            }
+
+            tbMainContent.Text = sb.ToString();
+        }
+    }
+}
---
+++ CilView/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿/* CIL Tools 
+ * Copyright (c) 2020, MSDN.WhiteKnight (https://github.com/MSDN-WhiteKnight) 
+ * License: BSD 2.0 */
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("CilView")]
+[assembly: AssemblyDescription(".NET bytecode visualizer")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CilView")]
+[assembly: AssemblyCopyright("Copyright © MSDN.WhiteKnight 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //где расположены словари ресурсов по конкретным тематикам
+    //(используется, если ресурс не найден на странице 
+    // или в словарях ресурсов приложения)
+    ResourceDictionaryLocation.SourceAssembly //где расположен словарь универсальных ресурсов
+    //(используется, если ресурс не найден на странице, 
+    // в приложении или в каких-либо словарях ресурсов для конкретной темы)
+)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
