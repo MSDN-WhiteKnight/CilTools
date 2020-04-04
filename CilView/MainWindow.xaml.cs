c66f9657223a2d349626a2ca882a833141f7d255
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2020-04-04 18:16:19 +0500

    Add basic UI to inspect processes

--- CilView/MainWindow.xaml.cs
+++ CilView/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
  * Copyright (c) 2020, MSDN.WhiteKnight (https://github.com/MSDN-WhiteKnight) 
  * License: BSD 2.0 */
 using System;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -10,15 +12,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using CilTools.BytecodeAnalysis;
 using CilTools.Runtime;
+using Microsoft.Diagnostics.Runtime;
 
 namespace CilView
 {
@@ -27,16 +23,21 @@ namespace CilView
     /// </summary>
     public partial class MainWindow : Window
     {
-        public static void DumpMethods(int pid, TextWriter target)
+        DataTarget dt = null;
+        ObservableCollection<Assembly> assemblies=new ObservableCollection<Assembly>();
+        ObservableCollection<Type> types = new ObservableCollection<Type>();
+        ObservableCollection<MethodBase> methods = new ObservableCollection<MethodBase>();
+
+        static void DumpMethods(int pid, TextWriter target)
         {
             Process process = Process.GetProcessById(pid);
             using (process)
             {
-                DumpMethods(process,target);
+                DumpMethods(process, target);
             }
         }
 
-        public static void DumpMethods(string processname, TextWriter target)
+        static void DumpMethods(string processname, TextWriter target)
         {
             Process[] processes = Process.GetProcessesByName(processname);
             if (processes.Length == 0)
@@ -53,7 +54,7 @@ namespace CilView
             }
         }
 
-        public static void DumpMethods(Process process, TextWriter target)
+        static void DumpMethods(Process process, TextWriter target)
         {
             //prints bytecode of methods in specified managed process
 
@@ -89,6 +90,79 @@ namespace CilView
             }
         }
 
+        ObservableCollection<Assembly> LoadAssemblies(int pid)
+        {
+            Process process = Process.GetProcessById(pid);
+            dt = DataTarget.AttachToProcess(process.Id, 5000, AttachFlag.Passive);
+
+            using (process)
+            {
+                return LoadAssemblies(dt);
+            }
+        }
+
+        ObservableCollection<Assembly> LoadAssemblies(string processname)
+        {
+            Process[] processes = Process.GetProcessesByName(processname);
+            if (processes.Length == 0)
+            {
+                MessageBox.Show("Process not found");
+                return new ObservableCollection<Assembly>();
+            }
+
+            Process process = processes[0];
+            dt = DataTarget.AttachToProcess(process.Id, 5000, AttachFlag.Passive);
+
+            using (process)
+            {
+                return LoadAssemblies(dt);
+            }
+        }
+
+
+        static ObservableCollection<Assembly> LoadAssemblies(DataTarget dt)
+        {
+            ObservableCollection<Assembly> ret = new ObservableCollection<Assembly>();
+
+            if (dt.ClrVersions.Count == 0)
+            {
+                MessageBox.Show("Error: unable to find .NET Runtime in target process!");
+                return ret;
+            }
+
+            var runtimeInfo = dt.ClrVersions[0];
+            ClrRuntime runtime = runtimeInfo.CreateRuntime();
+            ClrAssemblyReader reader = new ClrAssemblyReader(runtime);
+
+            foreach (ClrModule x in runtime.Modules)
+            {
+                ret.Add(reader.Read(x));
+            }
+
+            DynamicMethodsAssembly dynass = reader.GetDynamicMethods();
+            ret.Add(dynass);
+            return ret;
+        }
+
+        static ObservableCollection<Type> LoadTypes(Assembly ass)
+        {
+            ObservableCollection<Type> ret = new ObservableCollection<Type>(ass.GetTypes());
+            return ret;
+        }
+
+        static ObservableCollection<MethodBase> LoadMethods(Type t)
+        {
+            ObservableCollection<MethodBase> ret = new ObservableCollection<MethodBase>();
+            MemberInfo[] members = t.GetMembers(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
+
+            foreach (MemberInfo member in members)
+            {
+                if (member is MethodBase) ret.Add((MethodBase)member);
+            }
+
+            return ret;
+        }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -96,15 +170,59 @@ namespace CilView
 
         private void bShow_Click(object sender, RoutedEventArgs e)
         {
-            StringBuilder sb = new StringBuilder(2000);
+            if (this.dt != null)
+            {
+                this.dt.Dispose();
+                this.dt = null;
+            }
+
+            this.assemblies.Clear();
+            this.types.Clear();
+            this.methods.Clear();
+
+            this.assemblies = LoadAssemblies(tbProcessName.Text);
+            cbAssembly.ItemsSource = this.assemblies;
+        }
+
+        private void cbAssembly_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Assembly ass = (Assembly)cbAssembly.SelectedItem;
+
+            if (ass == null) return;
+
+            this.types.Clear();
+            this.methods.Clear();
+            this.types = LoadTypes(ass);
+            cbType.ItemsSource = this.types;
+        }
+
+        private void cbType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Type t = (Type)cbType.SelectedItem;
+
+            if (t == null) return;
+
+            this.methods.Clear();
+            this.methods = LoadMethods(t);
+            lbMethod.ItemsSource = this.methods;
+        }
+
+        private void lbMethod_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MethodBase mb = (MethodBase)lbMethod.SelectedItem;
+
+            if (mb == null) return;
+
+            StringBuilder sb = new StringBuilder(1000);
             StringWriter wr = new StringWriter(sb);
 
             using (wr)
             {
-                DumpMethods(tbProcessName.Text, wr);
+                CilGraph gr = CilAnalysis.GetGraph(mb);
+                gr.Print(wr, true, true, true, true);
+                wr.Flush();
+                tbMainContent.Text = sb.ToString();
             }
-
-            tbMainContent.Text = sb.ToString();
         }
     }
 }
