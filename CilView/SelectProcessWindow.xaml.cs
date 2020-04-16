b1785843c0a4a830b5431e1590f7f0477615d391
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2020-04-16 21:02:14 +0500

    Improve SelectProcessWindow
    - Sort processes alphabetically
    - Show error if no process is selected
    - Show details for selected process
    - Dispose processes on close

--- CilView/SelectProcessWindow.xaml.cs
+++ CilView/SelectProcessWindow.xaml.cs
@@ -22,6 +22,16 @@ namespace CilView
         {
             InitializeComponent();
             processes = Process.GetProcesses();
+            Array.Sort<Process>(processes, (x, y) => {
+                try
+                {
+                    return x.ProcessName.CompareTo(y.ProcessName);
+                }
+                catch (InvalidOperationException)
+                {
+                    return 0;
+                }
+            });
             lbProcesses.ItemsSource = processes;
         }
 
@@ -43,8 +53,60 @@ namespace CilView
 
         private void bOK_Click(object sender, RoutedEventArgs e)
         {
+            if (lbProcesses.SelectedItem == null)
+            {
+                MessageBox.Show(this,"Select process to attach from the list","Message");
+                return;
+            }
+
             this.DialogResult = true;
             this.Close();
+        }
+
+        private void lbProcesses_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Process pr = lbProcesses.SelectedItem as Process;
+
+            if (pr != null)
+            {
+                try
+                {
+                    string str_module;
+                    try
+                    {
+                        ProcessModule module;
+                        module = pr.MainModule;
+                        str_module = module.FileName;
+                    }
+                    catch (System.ComponentModel.Win32Exception)
+                    {
+                        str_module = "???";
+                    }
+
+                    tbInfo.Text = "PID: " + pr.Id.ToString() + "; Application: " + str_module;
+                }
+                catch (InvalidOperationException)
+                {
+                    tbInfo.Text = "???";
+                }
+            }
+            else
+            {
+                tbInfo.Text = "";
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if (this.processes != null)
+            {
+                for (int i = 0; i < processes.Length; i++)
+                {
+                    if (this.DialogResult == true && i == lbProcesses.SelectedIndex) continue;
+
+                    this.processes[i].Dispose();
+                }
+            }
         }
     }
 }
