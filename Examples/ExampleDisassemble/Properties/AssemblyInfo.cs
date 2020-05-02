658fc7429557728155d0c241756b11d6f4cb3a53
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2020-05-02 13:57:04 +0500

    Add example for CilTools.BytecodeAnalysis

---
+++ Examples/ExampleDisassemble/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿/* CIL Tools
+ * Copyright (c) 2020,  MSDN.WhiteKnight (https://github.com/MSDN-WhiteKnight) 
+ * License: BSD 2.0 */
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ExampleDisassemble")]
+[assembly: AssemblyDescription("CilTools.BytecodeAnalysis example")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CilTools")]
+[assembly: AssemblyCopyright("Copyright © MSDN.WhiteKnight 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("3dfa5a89-9a58-4502-aed1-50878e40b3ac")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Examples/ExampleDisassemble/readme.md
@@ -0,0 +1,3 @@
+## CIL Tools example - Disassemble method
+
+This example shows how to programmatically disassemble .NET method and display its CIL code using `GetCilText` extension method
