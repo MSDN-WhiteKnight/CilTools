e073b7dee5d96cd5fc5304a0108fb657f7e689f0
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2020-05-03 21:23:33 +0500

    Add license heading

--- CilView/CilVisualization.cs
+++ CilView/CilVisualization.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿/* CIL Tools 
+ * Copyright (c) 2020,  MSDN.WhiteKnight (https://github.com/MSDN-WhiteKnight) 
+ * License: BSD 2.0 */
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
