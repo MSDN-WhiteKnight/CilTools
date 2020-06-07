6f75d62738af630c0b9b8cac012a3525b4b2be29
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2020-06-07 13:54:27 +0500

    Fix issue when Assembly.LoadFile load wrong assembly from GAC

--- CilView/FileAssemblySource.cs
+++ CilView/FileAssemblySource.cs
@@ -13,6 +13,41 @@ namespace CilView
     {
         string _path; //assembly directory
 
+        Assembly LoadAssembly(string filepath)
+        {
+            Assembly main = null;
+
+            //try load to execution context
+            try
+            {
+                main = Assembly.LoadFile(filepath);
+            }
+            catch(BadImageFormatException) { }
+            catch(NotSupportedException) { }
+
+            if (main != null)
+            {
+                //Assembly.LoadFile can load wrong assembly from GAC
+
+                if (String.Equals(main.Location, filepath, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+                    return main;
+                }
+                else main = null;
+            }
+
+            //if failed, try reflection only context
+            main = Assembly.ReflectionOnlyLoadFrom(filepath);
+            
+            if (main != null)
+            {
+                AppDomain.CurrentDomain.ReflectionOnlyAssemblyResolve += CurrentDomain_ReflectionOnlyAssemblyResolve;
+                return main;
+            }
+            else return null;
+        }
+
         public FileAssemblySource(string filepath)
         {
             this._path = Path.GetDirectoryName(filepath);
@@ -20,20 +55,7 @@ namespace CilView
 
             Assembly main=null;
 
-            //try load to execution context
-            try 
-            { 
-                main = Assembly.LoadFile(filepath);
-                AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
-            }
-            catch (BadImageFormatException) { }
-
-            //if failed, try reflection only context
-            if (main == null)
-            {                
-                main = Assembly.ReflectionOnlyLoadFrom(filepath);
-                AppDomain.CurrentDomain.ReflectionOnlyAssemblyResolve += CurrentDomain_ReflectionOnlyAssemblyResolve;
-            }
+            main = this.LoadAssembly(filepath);
             
             if (main == null) throw new ApplicationException("Cannot load assembly " + filepath + " due to unknown error!");
 
@@ -41,8 +63,6 @@ namespace CilView
             this.Assemblies = ret;
             this.Types = new ObservableCollection<Type>();
             this.Methods = new ObservableCollection<MethodBase>();
-            
-            //FileLoadException
         }
 
         Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
@@ -51,7 +71,7 @@ namespace CilView
             string path = Path.Combine(this._path, an.Name + ".dll");
             Assembly ret = null;
 
-            //atempt to resolve assembly from current assembly directory
+            //attempt to resolve assembly from current assembly directory
 
             try
             {
@@ -60,6 +80,7 @@ namespace CilView
             catch (FileNotFoundException) { }
             catch (FileLoadException) { }
             catch (BadImageFormatException) { }
+            catch (NotSupportedException) { }
 
             if (ret != null) return ret;
 
