9a484eaca6dfbe0a902c6804977713644bdf571c
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2020-06-22 00:40:42 +0500

    Disable ClickOnce manifest signing

--- CilView/Properties/AssemblyInfo.cs
+++ CilView/Properties/AssemblyInfo.cs
@@ -27,5 +27,5 @@ using System.Windows;
     // в приложении или в каких-либо словарях ресурсов для конкретной темы)
 )]
 
-[assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyVersion("1.0.0.6")]
+[assembly: AssemblyFileVersion("1.0.0.6")]
