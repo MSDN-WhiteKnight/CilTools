2132d1f0765a3f23bfa92c0c6c8bdc519cbe69d7
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2020-11-21 13:17:53 +0500

    Upgrade CilTools.BytecodeAnalysis to SDK-style project format

--- CilTools.BytecodeAnalysis/pkg/package.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-cd %~dp0
-copy /Y /V ..\bin\Release\CilTools.BytecodeAnalysis.dll /B lib\net35\
-copy /Y /V ..\bin\Release\CilTools.BytecodeAnalysis.pdb /B lib\net35\
-copy /Y /V ..\bin\Release\CilTools.BytecodeAnalysis.xml lib\net35\
-copy /Y /V ..\ReadMe.txt .\
-"C:\Distr\Microsoft\nuget 2.8.6\nuget.exe" pack CilTools.BytecodeAnalysis.dll.nuspec
