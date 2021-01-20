ca35cea21e83bb83515908d0b5cea7a5424d694e
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2021-01-20 08:48:20 +0500

    Update Example3.cs

--- Examples/Example3.cs
+++ Examples/Example3.cs
@@ -1,10 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Reflection;
-using CilBytecodeParser;
-using CilBytecodeParser.Extensions;
+using CilTools.BytecodeAnalysis;
+using CilTools.BytecodeAnalysis.Extensions;
 
-namespace CilBytecodeParserTest
+namespace CilToolsTest
 {    
     class Program
     {
