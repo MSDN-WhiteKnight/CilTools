cf839f8bb03d1c9b765b8eab4d5e343121327ed5
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2021-02-27 18:14:29 +0500

    Convert EmitSampleApp to .NET SDK project format (#55)

--- tests/EmitSampleApp/EmitSampleProgram.cs
+++ tests/EmitSampleApp/EmitSampleProgram.cs
@@ -1,22 +1,20 @@
 ﻿/* CIL Tools 
- * Copyright (c) 2020,  MSDN.WhiteKnight (https://github.com/MSDN-WhiteKnight) 
+ * Copyright (c) 2021,  MSDN.WhiteKnight (https://github.com/MSDN-WhiteKnight) 
  * License: BSD 2.0 */
 using System;
-using System.Collections.Generic;
-using System.Reflection;
 using System.Reflection.Emit;
-using System.Text;
 
 namespace SampleApp
 {
     class EmitSampleProgram
     {
-        public static int x;
+        public static int x=0;
 
         static void Main(string[] args)
         {
-#if !NETSTANDARD
-            DynamicMethod dm = new DynamicMethod("Method1", typeof(int), new Type[] { typeof(int), typeof(int) }, typeof(EmitSampleProgram).Module);
+            DynamicMethod dm = new DynamicMethod(
+                "Method1", typeof(int), new Type[] { typeof(int), typeof(int) }, typeof(EmitSampleProgram).Module
+                );
             ILGenerator ilg = dm.GetILGenerator(512);
 
             ilg.BeginExceptionBlock();
@@ -34,7 +32,7 @@ namespace SampleApp
             Func<int, int, int> deleg = (Func<int, int, int>)dm.CreateDelegate(typeof(Func<int, int, int>));
             int res = deleg(1,2);
             Console.WriteLine(res.ToString());
-#endif
+
             Console.ReadLine();
         }
     }
