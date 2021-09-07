ebbca1c2048396cb0c3deeed8d40d1e8fc80c91b
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2021-09-07 08:26:43 +0500

    Update MemoryImageTests.cs
    Ensure image is loaded to memory

--- tests/CilTools.Metadata.Tests/MemoryImageTests.cs
+++ tests/CilTools.Metadata.Tests/MemoryImageTests.cs
@@ -1,4 +1,4 @@
-﻿/* CilTools.Metadata tests
+/* CilTools.Metadata tests
  * Copyright (c) 2021,  MSDN.WhiteKnight (https://github.com/MSDN-WhiteKnight) 
  * License: BSD 2.0 */
 using System;
@@ -15,34 +15,63 @@ namespace CilTools.Metadata.Tests
     [TestClass]
     public class MemoryImageTests
     {
+    
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr LoadLibraryEx(string lpFileName, IntPtr hReservedNull, LoadLibraryFlags dwFlags);
+        
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MODULEINFO
+        {
+            public IntPtr lpBaseOfDll;
+            public uint SizeOfImage;
+            public IntPtr EntryPoint;
+        }
+
+        [DllImport("psapi.dll", SetLastError = true)]
+        static extern bool GetModuleInformation(IntPtr hProcess, IntPtr hModule, out MODULEINFO lpmodinfo, uint cb);
+        
+        [DllImport("kernel32.dll", SetLastError = true, EntryPoint = "ReadProcessMemory")]
+        static extern bool ReadProcessMemory_Byte(
+            IntPtr hProcess,IntPtr lpBaseAddress,out byte lpBuffer,int dwSize,out IntPtr lpNumberOfBytesRead
+            );
+    
         [TestMethod]
         public void Test_MemoryImage_Load()
         {
             //find corelib module
+            string path = typeof(object).Assembly.Location;
             string assname = typeof(object).Assembly.GetName().Name;
-            string name = assname+".ni.dll";
-            ProcessModule module = null;
-            ProcessModuleCollection pmc = Process.GetCurrentProcess().Modules;
+            string name = assname+".dll";
             
-            foreach (ProcessModule m in pmc)
-            {
-                Logger.LogMessage(m.ModuleName);
-
-                if (m.ModuleName.Equals(name, StringComparison.OrdinalIgnoreCase)) 
-                {
-                    module = m;
-                    break;
-                }
-            }
-
-            if (module == null) Assert.Fail(name+" module not found");
+            IntPtr hProcess = Process.GetCurrentProcess().Handle;
+            Assert.AreNotEqual(IntPtr.Zero,hProcess);
+            
+            IntPtr hModule = LoadLibraryEx(path, IntPtr.Zero, 0);
+            if (hModule == IntPtr.Zero) throw new Win32Exception(Marshal.GetLastWin32Error());
+            
+            MODULEINFO mi = new MODULEINFO();
+            bool res = GetModuleInformation(hProcess, hModule, out mi, (uint)Marshal.SizeOf(mi));
+            if (res == false) throw new Win32Exception(Marshal.GetLastWin32Error());
 
             //get corelib image as byte array
-            byte[] imageBytes = new byte[module.ModuleMemorySize];
-            Marshal.Copy(module.BaseAddress, imageBytes, 0, module.ModuleMemorySize);
+            byte[] data = new byte[mi.SizeOfImage];
+
+            IntPtr c;
+            int count_errors = 0;
+            
+            for (int i = 0; i < data.Length; i++)
+            {
+                byte b = 0;
+                res = ReadProcessMemory_Byte(hProcess, mi.lpBaseOfDll + i, out b, 1, out c);
+
+                if (res == false || c == IntPtr.Zero) count_errors++;
+                else data[i] = b;
+            }
+            
+            Assert.IsTrue(count_errors<data.Length);
 
             //load memory image
-            MemoryImage img = new MemoryImage(imageBytes, module.FileName);
+            MemoryImage img = new MemoryImage(data, path);
 
             AssemblyReader reader = new AssemblyReader();
 
