178ea61acc5be8f29ad63be16f475278ab552e34
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2021-10-14 20:15:55 +0500

    Update OpenCodeWindow
    - Add context help
    - Add Insert and From file.. buttons

--- CilView/UI.Dialogs/OpenCodeWindow.xaml.cs
+++ CilView/UI.Dialogs/OpenCodeWindow.xaml.cs
@@ -3,15 +3,27 @@
  * License: BSD 2.0 */
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using CilView.Build;
+using Microsoft.Win32;
 
 namespace CilView.UI.Dialogs
 {
     public partial class OpenCodeWindow : Window
     {
+        const string contextHelp = 
+            "This dialog enables you to view disassembled CIL code corresponding to the entered code snippet. " +
+            "It only supports a self-contained code snippet without any dependencies or special build options; open an " +
+            "MSBuild project file (.csproj/.vbproj) instead if you need more complex scenarios.\r\n\r\n" +
+            "CIL View will compile the provided code in background, and open the output binary if the " +
+            "compilation is successful. " +
+            "The code is compiled as a class library in DEBUG mode, with optimizations off. " +
+            "Unsafe code is allowed in C#.";
+
         LangItem[] langs;
 
         public string Code 
@@ -72,6 +84,78 @@ public class Program
 
             this.DialogResult = true;
             this.Close();
+        }
+
+        private void cbLang_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (this.SelectedLanguage) 
+            {
+                case CodeLanguage.CSharp:
+                    tbVersionInfo.Text = "Supports C# 5.0 targeting .NET Framework 4.5";
+                    break;
+                case CodeLanguage.VB:
+                    tbVersionInfo.Text = "Supports VB.NET 11.0 targeting .NET Framework 4.5";
+                    break;
+            }
+        }
+
+        private void labelHelp_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton != MouseButtonState.Pressed) return;
+
+            TextViewWindow wnd = new TextViewWindow();
+            wnd.Owner = this;
+            wnd.Title = "About Open code dialog";
+            wnd.Text = contextHelp;
+            wnd.ShowDialog();
+        }
+
+        private void bInsert_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (Clipboard.ContainsText())
+                {
+                    string text = Clipboard.GetText();
+
+                    if (string.IsNullOrWhiteSpace(text)) return;
+                    
+                    tbContent.SelectedText = text;
+                    tbContent.Focus();
+                }
+            }
+            catch (Exception ex) 
+            {
+                ErrorHandler.Current.Error(ex);
+            }
+        }
+
+        private void bFromFile_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dlg = new OpenFileDialog();
+                dlg.RestoreDirectory = true;
+                dlg.Filter = "Code files (*.cs,*.vb)|*.cs;*.vb|All files|*";
+
+                if (dlg.ShowDialog(this) != true) return;
+
+                string file = dlg.FileName;
+                string text = File.ReadAllText(file);
+
+                if (!string.IsNullOrWhiteSpace(text))
+                {
+                    tbContent.Text = text;
+                }
+                else
+                {
+                    MessageBox.Show(this, "File is empty", "Error");
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorHandler.Current.Error(ex);
+            }
         }
     }
 }
