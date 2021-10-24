9d5e07eb855bf16c53a0edb854201e5deca54d0f
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2021-10-24 22:20:43 +0500

    Update SourceViewWindow
    Add buttons to navigate to previous/next sequence point

--- CilView/UI.Dialogs/SourceViewWindow.xaml.cs
+++ CilView/UI.Dialogs/SourceViewWindow.xaml.cs
@@ -16,9 +16,8 @@ namespace CilView.UI.Dialogs
     {
         SourceInfo _info;
 
-        public SourceViewWindow(SourceInfo info)
+        void LoadSourceInfo(SourceInfo info)
         {
-            InitializeComponent();
             this._info = info;
 
             StringBuilder method_str = new StringBuilder(200);
@@ -51,6 +50,19 @@ namespace CilView.UI.Dialogs
             }
 
             tbSource.Text = info.SourceCode;
+
+            if (info.CilStart == 0) bPrevious.IsEnabled = false;
+            else bPrevious.IsEnabled = true;
+
+            int size = Utils.GetMethodBodySize(info.Method);
+            if (info.CilEnd >= size) bNext.IsEnabled = false;
+            else bNext.IsEnabled = true;
+        }
+
+        public SourceViewWindow(SourceInfo info)
+        {
+            InitializeComponent();
+            this.LoadSourceInfo(info);
         }
 
         private void bClose_Click(object sender, RoutedEventArgs e)
@@ -63,8 +75,61 @@ namespace CilView.UI.Dialogs
             if (e.LeftButton != MouseButtonState.Pressed) return;
 
             //open the source file in external editor
-            Utils.ShellExecute(this._info.SourceFile, this, 
+            Utils.ShellExecute(this._info.SourceFile, this,
                 "Failed to open source code file");
+        }
+
+        private void bPrevious_Click(object sender, RoutedEventArgs e)
+        {
+            uint offset = this._info.CilStart;
+            if (offset == 0) return;
+
+            offset--;
+
+            try
+            {
+                SourceInfo info_new = PdbUtils.GetSourceFromPdb(this._info.Method, offset, offset,
+                    SymbolsQueryType.SequencePoint);
+
+                if (string.IsNullOrEmpty(info_new.SourceCode))
+                {
+                    MessageBox.Show("Failed to get the previous sequence point");
+                    return;
+                }
+
+                this.LoadSourceInfo(info_new);
+            }
+            catch (Exception ex)
+            {
+                ErrorHandler.Current.Error(ex);
+            }
+        }
+
+        private void bNext_Click(object sender, RoutedEventArgs e)
+        {
+            int offset = (int)this._info.CilEnd;
+            int size = Utils.GetMethodBodySize(this._info.Method);
+            if (offset >= size) return;
+
+            offset++;
+
+            try
+            {
+                SourceInfo info_new = PdbUtils.GetSourceFromPdb(this._info.Method, (uint)offset, (uint)offset,
+                    SymbolsQueryType.SequencePoint);
+
+                if (string.IsNullOrEmpty(info_new.SourceCode))
+                {
+                    MessageBox.Show("Failed to get the next sequence point");
+                    return;
+                }
+
+                this.LoadSourceInfo(info_new);
+            }
+            catch (Exception ex)
+            {
+                ErrorHandler.Current.Error(ex);
+            }
         }
     }
 }
