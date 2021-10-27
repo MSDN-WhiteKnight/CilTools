7e320d0c21fc169c3376649ba18241945c8368e0
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2021-10-27 22:27:36 +0500

    Add Portable PDB reader

---
+++ CilView/Internal.Pdb.Readers/Portable/PortablePdb.cs
@@ -0,0 +1,100 @@
+﻿/* CIL Tools 
+ * Copyright (c) 2021, MSDN.WhiteKnight (https://github.com/MSDN-WhiteKnight) 
+ * License: BSD 2.0 */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Internal.Pdb.Portable
+{
+    static class PortablePdb
+    {
+        static string ReadDocumentPath(MetadataReader reader, DocumentHandle dh)
+        {
+            Document doc = reader.GetDocument(dh);
+            BlobReader blob = reader.GetBlobReader(doc.Name);
+            char separator = (char)blob.ReadByte();
+            StringBuilder sb = new StringBuilder(300);
+
+            while (true)
+            {
+                BlobHandle bh = blob.ReadBlobHandle();
+                if (bh.IsNil) break;
+
+                byte[] nameBytes = reader.GetBlobBytes(bh);
+                sb.Append(Encoding.UTF8.GetString(nameBytes));
+
+                if (blob.Offset >= blob.Length) break;
+
+                sb.Append(separator);
+            }
+
+            string path = sb.ToString();
+            return path;
+        }
+
+        public static IEnumerable<SourceLineData> GetSourceLineData(string pdbPath, int methodToken)
+        {
+            //determine method row number
+            EntityHandle ehMethod = MetadataTokens.EntityHandle(methodToken);
+
+            if (ehMethod.Kind != HandleKind.MethodDefinition &&
+                ehMethod.Kind != HandleKind.MethodDebugInformation)
+            {
+                throw new ArgumentException("Invalid token: " + ehMethod.Kind.ToString(), "methodToken");
+            }
+
+            int rowNumber = MetadataTokens.GetRowNumber(ehMethod);
+
+            //MethodDebugInformation row number is the same
+            MethodDebugInformationHandle hDebug = MetadataTokens.MethodDebugInformationHandle(rowNumber);
+
+            //open Portable PDB file
+            FileStream fs = new FileStream(pdbPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            MetadataReaderProvider provider = MetadataReaderProvider.FromPortablePdbStream(fs);
+
+            using (provider)
+            {
+                //stream is disposed by MetadataReaderProvider
+                MetadataReader reader = provider.GetMetadataReader();
+
+                if (rowNumber > reader.MethodDebugInformation.Count)
+                {
+                    throw new ArgumentOutOfRangeException("methodToken");
+                }
+
+                MethodDebugInformation di = reader.GetMethodDebugInformation(hDebug);
+                string path = string.Empty;
+
+                //file path if all sequence points are in the same file
+                if (!di.Document.IsNil) path = ReadDocumentPath(reader, di.Document);
+
+                foreach (SequencePoint sp in di.GetSequencePoints())
+                {
+                    if (sp.IsHidden) continue;
+
+                    string s = path;
+
+                    if (!sp.Document.IsNil && sp.Document != di.Document)
+                    {
+                        //file path if method spans over nultiple files
+                        s = ReadDocumentPath(reader, di.Document);
+                    }
+
+                    yield return new SourceLineData()
+                    {
+                        FilePath = s,
+                        CilOffset = sp.Offset,
+                        LineStart = sp.StartLine,
+                        LineEnd = sp.EndLine,
+                        ColStart = sp.StartColumn,
+                        ColEnd = sp.EndColumn
+                    };
+                }
+            }//end using
+        }
+    }
+}
---
+++ CilView/Internal.Pdb.Readers/Portable/SourceLineData.cs
@@ -0,0 +1,17 @@
+﻿/* CIL Tools 
+ * Copyright (c) 2021, MSDN.WhiteKnight (https://github.com/MSDN-WhiteKnight) 
+ * License: BSD 2.0 */
+using System;
+
+namespace Internal.Pdb.Portable
+{
+    class SourceLineData
+    {
+        public string FilePath { get; set; }
+        public int CilOffset { get; set; }
+        public int LineStart { get; set; }
+        public int LineEnd { get; set; }
+        public int ColStart { get; set; }
+        public int ColEnd { get; set; }
+    }
+}
