f3442298a6745195f031154c90a80681e59b7eb1
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2021-12-17 13:30:18 +0500

    Update Program.cs
    - Use CilTools.Metadata
    - Add --nocolor option
    - Return non-zero value from process on errors
    - Format code

--- CilTools.CommandLine/Program.cs
+++ CilTools.CommandLine/Program.cs
@@ -3,12 +3,10 @@
  * License: BSD 2.0 */
 using System;
 using System.Collections.Generic;
-using System.Reflection;
-using System.Reflection.Emit;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using CilTools.BytecodeAnalysis;
-using CilTools.BytecodeAnalysis.Extensions;
 using CilTools.Metadata;
 using CilTools.Syntax;
 
@@ -22,149 +20,216 @@ namespace CilTools.CommandLine
             Console.WriteLine(" Commands:");
             Console.WriteLine();
             Console.WriteLine("view - Print CIL code of the method or methods with the specified name");
-            Console.WriteLine("[Usage: "+exeName+" view (assembly path) (type full name) (method name)]");
+            Console.WriteLine("Usage: " + exeName + " view [--nocolor] <assembly path> <type full name> <method name>");
+            Console.WriteLine("[--nocolor] - disable syntax highlighting");
             Console.WriteLine();
             Console.WriteLine("help - Print available commands");
             Console.WriteLine();
         }
-        
+
         static string GetErrorInfo()
         {
             string exeName = typeof(Program).Assembly.GetName().Name;
-            return "Use \""+exeName+" help\" to print the list of available commands and their arguments.";
+            return "Use \"" + exeName + " help\" to print the list of available commands and their arguments.";
         }
-        
-    static void PrintNode(SyntaxNode node)
-    {
-        //recursively prints CIL syntax tree to console
 
-        SyntaxNode[] children = node.GetChildNodes();
-
-        if (children.Length == 0)
+        static void PrintNode(SyntaxNode node, bool noColor)
         {
-            //if it a leaf node, print its content to console
-            ConsoleColor originalColor=Console.ForegroundColor;
-            
-            //hightlight syntax elements
+            //recursively prints CIL syntax tree to console
 
-            if (node is KeywordSyntax) 
+            SyntaxNode[] children = node.GetChildNodes();
+
+            if (children.Length == 0)
             {
-                KeywordSyntax ks = (KeywordSyntax)node;
-                
-                if (ks.Kind == KeywordKind.Other) Console.ForegroundColor = ConsoleColor.Blue;
-                else if (ks.Kind == KeywordKind.DirectiveName) Console.ForegroundColor = ConsoleColor.Magenta;
+                //if it a leaf node, print its content to console
+
+                if (noColor)
+                {
+                    //no syntax highlighting
+                    node.ToText(Console.Out);
+                    return;
+                }
+
+                //hightlight syntax elements
+                ConsoleColor originalColor = Console.ForegroundColor;
+
+                if (node is KeywordSyntax)
+                {
+                    KeywordSyntax ks = (KeywordSyntax)node;
+
+                    if (ks.Kind == KeywordKind.Other) Console.ForegroundColor = ConsoleColor.Blue;
+                    else if (ks.Kind == KeywordKind.DirectiveName) Console.ForegroundColor = ConsoleColor.Magenta;
+                }
+                else if (node is IdentifierSyntax)
+                {
+                    IdentifierSyntax id = (IdentifierSyntax)node;
+
+                    if (id.IsMemberName) Console.ForegroundColor = ConsoleColor.Cyan;
+                }
+                else if (node is LiteralSyntax)
+                {
+                    LiteralSyntax lit = (LiteralSyntax)node;
+                    if (lit.Value is string) Console.ForegroundColor = ConsoleColor.Red;
+                }
+                else if (node is CommentSyntax)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+
+                node.ToText(Console.Out);
+
+                //restore console color to default value
+                Console.ForegroundColor = originalColor;
             }
-            else if (node is IdentifierSyntax)
+            else
             {
-                IdentifierSyntax id = (IdentifierSyntax)node;
-                
-                if (id.IsMemberName) Console.ForegroundColor = ConsoleColor.Cyan;
+                //if the node has child nodes, process them
+
+                for (int i = 0; i < children.Length; i++)
+                {
+                    PrintNode(children[i], noColor);
+                }
             }
-            else if (node is LiteralSyntax)
-            {
-                LiteralSyntax lit = (LiteralSyntax)node;                
-                if (lit.Value is string) Console.ForegroundColor = ConsoleColor.Red;
-            }
-            else if (node is CommentSyntax)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-            }
-            
-            node.ToText(Console.Out);
-            
-            //restore console color to default value
-            Console.ForegroundColor = originalColor;
         }
-        else
-        {
-            //if the node has child nodes, process them
 
-            for (int i = 0; i < children.Length; i++)
-            {
-                PrintNode(children[i]);
-            }
-        }
-    }
-
-        static void PrintMethod(MethodBase method)
+        static void PrintMethod(MethodBase method, bool noColor)
         {
-            CilGraph graph = CilGraph.Create(method);                
+            CilGraph graph = CilGraph.Create(method);
             SyntaxNode root = graph.ToSyntaxTree();
-            PrintNode(root);
+            PrintNode(root, noColor);
         }
-        
+
         static bool TryReadExpectedParameter(string[] args, int pos, string expected)
         {
-            if(pos >= args.Length) return false;
-            
-            if(args[pos] == expected) return true;
+            if (pos >= args.Length) return false;
+
+            if (args[pos] == expected) return true;
             else return false;
         }
-        
+
         static string ReadCommandParameter(string[] args, int pos)
         {
-            return "";
+            if (pos >= args.Length) return null;
+
+            return args[pos];
         }
-    
-        static void Main(string[] args)
+
+        static int Main(string[] args)
         {
             Console.WriteLine("*** CIL Tools command line ***");
             Console.WriteLine("Copyright (c) 2021, MSDN.WhiteKnight (https://github.com/MSDN-WhiteKnight/CilTools)");
             Console.WriteLine();
-            
+
             string asspath;
             string type;
             string method;
             Assembly ass;
-            
-                if (args.Length == 0)
+            bool noColor = false;
+
+            //read and validate command line arguments
+
+            if (args.Length == 0)
+            {
+                PrintHelp();
+                return 1;
+            }
+            else if (args[0] == "help" || args[0] == "/?" || args[0] == "-?")
+            {
+                PrintHelp();
+                return 0;
+            }
+            else if (args[0] == "view")
+            {
+                if (args.Length < 4)
                 {
-                    PrintHelp();
-                    return;
-                }
-                else if(args[0]=="help" || args[0]=="/?" || args[0]=="-?")
-                {
-                    PrintHelp();
-                    return;
-                }
-                else if(args[0]=="view")
-                {
-                    if(args.Length<4)
-                    {
-                        Console.WriteLine("Error: not enough arguments for 'view' command.");
-                        Console.WriteLine(GetErrorInfo());
-                        return;
-                    }
-                    
-                    asspath = args[1];
-                    type = args[2];
-                    method = args[3];
-                    Console.WriteLine("Assembly: "+asspath);
-                    ass = Assembly.LoadFrom(asspath);
-                }
-                else
-                {
-                    Console.WriteLine("Error: unknown command "+args[0]);
+                    Console.WriteLine("Error: not enough arguments for 'view' command.");
                     Console.WriteLine(GetErrorInfo());
-                    return;
+                    return 1;
                 }
 
-                Console.WriteLine("{0}.{1}", type, method);
-                Console.WriteLine();
+                int pos = 1;
 
+                if (TryReadExpectedParameter(args, pos, "--nocolor"))
+                {
+                    noColor = true;
+                    pos++;
+                }
+
+                asspath = ReadCommandParameter(args, pos);
+                pos++;
+
+                if (string.IsNullOrEmpty(asspath))
+                {
+                    Console.WriteLine("Error: Assembly path is not provided for the 'view' command.");
+                    Console.WriteLine(GetErrorInfo());
+                    return 1;
+                }
+
+                type = ReadCommandParameter(args, pos);
+                pos++;
+
+                if (type == null)
+                {
+                    Console.WriteLine("Error: Type name is not provided for the 'view' command.");
+                    Console.WriteLine(GetErrorInfo());
+                    return 1;
+                }
+
+                method = ReadCommandParameter(args, pos);
+
+                if (string.IsNullOrEmpty(method))
+                {
+                    Console.WriteLine("Error: Method name is not provided for the 'view' command.");
+                    Console.WriteLine(GetErrorInfo());
+                    return 1;
+                }
+
+                Console.WriteLine("Assembly: " + asspath);
+            }
+            else
+            {
+                Console.WriteLine("Error: unknown command " + args[0]);
+                Console.WriteLine(GetErrorInfo());
+                return 1;
+            }
+
+            //view command implementation
+            Console.WriteLine("{0}.{1}", type, method);
+            Console.WriteLine();
+            AssemblyReader reader = new AssemblyReader();
+            int retCode;
+
+            try
+            {
+                ass = reader.LoadFrom(asspath);
                 Type t = ass.GetType(type);
 
                 MemberInfo[] methods = t.GetMembers(
                     BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static
                     );
 
-            MethodBase[] selectedMethods = methods.OfType<MethodBase>().Where((x) => { return x.Name == method; }).ToArray();
-            
-            for(int i=0;i<selectedMethods.Length;i++)
-            {
-                PrintMethod(selectedMethods[i]);
-                Console.WriteLine();
+                MethodBase[] selectedMethods = methods.OfType<MethodBase>().Where((x) => { return x.Name == method; }).ToArray();
+
+                for (int i = 0; i < selectedMethods.Length; i++)
+                {
+                    PrintMethod(selectedMethods[i], noColor);
+                    Console.WriteLine();
+                }
+
+                retCode = 0;
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error:");
+                Console.WriteLine(ex.ToString());
+                retCode = 1;
+            }
+            finally
+            {
+                reader.Dispose();
+            }
+
+            return retCode;
         }
     }
 }
