9bfee2d86dd473a431df8f21b9827dc8c55cc2fe
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2021-12-17 14:02:03 +0500

    Set assembly name

--- CilTools.CommandLine/run.bat
+++ CilTools.CommandLine/run.bat
@@ -1,3 +1,3 @@
-dotnet build -f netcoreapp3.1
-.\bin\Debug\netcoreapp3.1\CilTools.CommandLine.exe view "..\tests\CilTools.BytecodeAnalysis.Tests\bin\Debug\netcoreapp2.1\CilTools.Tests.Common.dll" CilTools.Tests.Common.SampleMethods PrintHelloWorld
+dotnet build
+.\bin\Debug\netcoreapp3.1\cil.exe view "..\tests\CilTools.BytecodeAnalysis.Tests\bin\Debug\netcoreapp2.1\CilTools.Tests.Common.dll" CilTools.Tests.Common.SampleMethods PrintHelloWorld
 pause
