93363b0b15a928243df069ddd769a7036b0cfcf2
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2021-12-26 13:26:49 +0500

    Add MethodSpecTests

---
+++ tests/CilTools.Metadata.Tests/MethodSpecTests.cs
@@ -0,0 +1,72 @@
+﻿/* CilTools.Metadata tests
+ * Copyright (c) 2021,  MSDN.WhiteKnight (https://github.com/MSDN-WhiteKnight) 
+ * License: BSD 2.0 */
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using CilTools.BytecodeAnalysis;
+using CilTools.Reflection;
+using CilTools.Syntax;
+using CilTools.Tests.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CilTools.Metadata.Tests
+{
+    [TestClass]
+    public class MethodSpecTests
+    {
+        static MethodBase GetMethodSpec()
+        {
+            Assembly ass = MethodRefTests.Reader.LoadFrom(typeof(SampleMethods).Assembly.Location);
+            Type t = ass.GetType("CilTools.Tests.Common.SampleMethods");
+            MethodBase mb = t.GetMember("GenericsTest")[0] as MethodBase;
+
+            CilGraph graph = CilGraph.Create(mb);
+            CilGraphNode[] nodes = graph.GetNodes().ToArray();
+
+            //find called GenerateArray method
+            MethodBase called = null;
+
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                if (nodes[i].Instruction.OpCode.FlowControl == FlowControl.Call)
+                {
+                    MethodBase target = nodes[i].Instruction.ReferencedMember as MethodBase;
+
+                    if (string.Equals(target.Name, "GenerateArray", StringComparison.InvariantCulture))
+                    {
+                        called = target;
+                        break;
+                    }
+                }
+            }
+
+            Assert.IsNotNull(called, "GenericsTest should contain a call to GenerateArray");
+            return called;
+        }
+
+        [TestMethod]
+        public void Test_MethodSpec_GenericMethodParameter()
+        {
+            //Verify that generic method parameter operand does not lose its connection with generic 
+            //method definition when obtained from instantiation
+            MethodBase m = GetMethodSpec();
+
+            CilGraph graph = CilGraph.Create(m);
+            CilGraphNode[] nodes = graph.GetNodes().ToArray();
+
+            //newarr !!T
+            CilGraphNode node = nodes.Where((x) => x.Instruction.OpCode == OpCodes.Newarr).Single();
+            Type t = node.Instruction.ReferencedType;
+
+            Assert.IsTrue(t.IsGenericParameter);
+            Assert.AreEqual(0, t.GenericParameterPosition);
+            Assert.AreEqual("GenerateArray", t.DeclaringMethod.Name);
+            Assert.AreEqual("T", t.Name);
+        }
+    }
+}
