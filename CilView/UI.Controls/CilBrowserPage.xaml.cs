22c1766b50098fba010842bf843a06d6753bf0d6
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2022-06-21 10:46:50 +0500

    Disable formatted view for large .il files

--- CilView/UI.Controls/CilBrowserPage.xaml.cs
+++ CilView/UI.Controls/CilBrowserPage.xaml.cs
@@ -83,7 +83,18 @@ namespace CilView.UI.Controls
             
             this.tbMainContent.Text = contentText;
             gridContent.Children.Clear();
-            gridContent.Children.Add(CilVisualization.VisualizeSourceText(contentText));
+            
+            if (contentText.Length < 1024*1024)
+            {
+                gridContent.Children.Add(CilVisualization.VisualizeSourceText(contentText));
+            }
+            else
+            {
+                TextBlock txt = new TextBlock();
+                txt2.Text = "[Error: Formatted view is not supported for files larger then 1 MB]";
+                gridContent.Children.Add(txt);
+            }
+            
             this.tbCurrLocation.Text = filename;
         }
 
