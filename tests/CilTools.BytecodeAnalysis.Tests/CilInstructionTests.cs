854517660a38931c275e4bf4bde63b15fa8c1292
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2022-07-14 21:15:38 +0500

    Move more tests from common project

--- tests/CilTools.BytecodeAnalysis.Tests/CilInstructionTests.cs
+++ tests/CilTools.BytecodeAnalysis.Tests/CilInstructionTests.cs
@@ -1,9 +1,11 @@
 ﻿/* CilTools.BytecodeAnalysis library tests
- * Copyright (c) 2021,  MSDN.WhiteKnight (https://github.com/MSDN-WhiteKnight) 
+ * Copyright (c) 2022,  MSDN.WhiteKnight (https://github.com/MSDN-WhiteKnight) 
  * License: BSD 2.0 */
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using System.Reflection.Emit;
 using CilTools.BytecodeAnalysis;
 using CilTools.Tests.Common;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -16,14 +18,47 @@ namespace CilTools.BytecodeAnalysis.Tests
         [TestMethod]
         public void Test_CilInstruction_Roundtrip()
         {
-            CilInstructionTestsCore.Test_CilInstruction_Roundtrip();
+            CilInstruction instr = CilInstruction.CreateEmptyInstruction(null);
+            string str = instr.ToString();
+            Assert.IsTrue(str.Contains("nop"), "The result of instr.ToString() should contain instruction name");
+            CilInstruction instr2 = CilInstruction.Parse(str);
+            Assert.AreEqual<OpCode>(OpCodes.Nop, instr2.OpCode, "The result of CilInstruction.Parse doesn't have expected opcode");
+            Assert.IsNull(instr2.Operand, "The 'nop' instruction should not have operand");
+
+            instr = CilInstruction.Create<int>(OpCodes.Ldc_I4, 1, sizeof(int));
+            str = instr.ToString();
+            Assert.IsTrue(str.Contains("ldc.i4"), "The result of instr.ToString() should contain instruction name");
+            AssertThat.IsMatch(str, new Text[] { "ldc.i4", Text.Whitespace, "1" });
+            instr2 = CilInstruction.Parse(str);
+            Assert.AreEqual<OpCode>(OpCodes.Ldc_I4, instr2.OpCode, "The result of CilInstruction.Parse doesn't have expected opcode");
+            Assert.AreEqual<int>(1, (int)instr2.Operand, "The result of CilInstruction.Parse doesn't have expected operand");
         }
 
         [TestMethod]
         [MethodTestData(typeof(SampleMethods), "PrintHelloWorld", BytecodeProviders.All)]
         public void Test_CilInstruction_ToString(MethodBase mi)
         {
-            CilInstructionTestsCore.Test_CilInstruction_ToString(mi);
+            CilInstruction[] instructions = CilReader.GetInstructions(mi).ToArray();
+
+            //verify that array contains a single instruction which .ToString() output
+            //matches the specified text pattern
+
+            AssertThat.HasOnlyOneMatch( instructions,
+                (x) => {
+                    string s = x.ToString();
+
+                    //call void [mscorlib]System.Console::WriteLine(string)
+
+                    return Text.IsMatch(s, new Text[] {
+                        "call",Text.Whitespace,
+                        "void",Text.Any,
+                        "System.Console::WriteLine",Text.Any,
+                        "(",Text.Any,
+                        "string",Text.Any,
+                        ")",Text.Any,
+                    });
+                }
+            );
         }
     }
 }
