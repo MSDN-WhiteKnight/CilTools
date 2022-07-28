12ee30be4c0892fd311ce4b83720fd5d8ac13efb
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2022-07-28 09:51:07 +0500

    Update test script

--- scripts/netstd/test.ps1
+++ scripts/netstd/test.ps1
@@ -1,4 +1,5 @@
 $toTest = "tests/CilTools.BytecodeAnalysis.Tests/CilTools.BytecodeAnalysis.Tests.csproj -f netcoreapp3.1",
+          "tests/CilTools.Metadata.Tests/CilTools.Metadata.Tests.csproj -f netcoreapp3.1",
           "tests/CilTools.CommandLine.Tests/CilTools.CommandLine.Tests.csproj",
           "tests/CilView.Tests/CilView.Tests.csproj"
 
