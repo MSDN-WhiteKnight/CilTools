bfed2715e66f897bd357bc93f15b87d649584575
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2022-08-06 14:46:23 +0500

    Add SourceLinkMap

--- CilView/SourceCode/SourceLinkEntry.cs
+++ CilView/SourceCode/SourceLinkEntry.cs
@@ -11,5 +11,40 @@ namespace CilView.SourceCode
     {
         public string SymbolsPath { get; set; }
         public string ServerPath { get; set; }
+
+        public string GetServerPath(string symbolsPath)
+        {
+            if (this.SymbolsPath.EndsWith("*", StringComparison.Ordinal))
+            {
+                //wildcard
+                string matchPrefix = this.SymbolsPath.Substring(0, this.SymbolsPath.Length - 1);
+
+                if (symbolsPath.StartsWith(matchPrefix, StringComparison.Ordinal))
+                {
+                    if(matchPrefix.Length >= symbolsPath.Length) return string.Empty;
+
+                    string symbolsPathSuffix = symbolsPath.Substring(matchPrefix.Length);
+                    string mappedPath = this.ServerPath.Replace("*", symbolsPathSuffix);
+                    mappedPath = mappedPath.Replace("\\", "/");
+                    return mappedPath;
+                }
+                else
+                {
+                    return string.Empty;
+                }
+            }
+            else
+            {
+                //exact
+                if (this.SymbolsPath.Equals(symbolsPath, StringComparison.Ordinal))
+                {
+                    return this.ServerPath;
+                }
+                else
+                {
+                    return string.Empty;
+                }
+            }
+        }
     }
 }
---
+++ CilView/SourceCode/SourceLinkMap.cs
@@ -0,0 +1,54 @@
+﻿/* CIL Tools 
+ * Copyright (c) 2022, MSDN.WhiteKnight (https://github.com/MSDN-WhiteKnight) 
+ * License: BSD 2.0 */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace CilView.SourceCode
+{
+    public class SourceLinkMap
+    {
+        SourceLinkEntry[] entries;
+
+        static readonly SourceLinkMap Empty = new SourceLinkMap(new SourceLinkEntry[0]);
+
+        SourceLinkMap(SourceLinkEntry[] e)
+        {
+            this.entries = e;
+        }
+        
+        public string GetServerPath(string symbolsPath)
+        {
+            for (int i = 0; i < this.entries.Length; i++)
+            {
+                string serverpath = this.entries[i].GetServerPath(symbolsPath);
+
+                if (serverpath.Length > 0) return serverpath;
+            }
+
+            return string.Empty;
+        }
+
+        public static SourceLinkMap Read(string json)
+        {
+            JObject j = JObject.Parse(json);
+            JObject docs = j["documents"] as JObject;
+
+            if (docs == null) return Empty;
+
+            List<SourceLinkEntry> ret = new List<SourceLinkEntry>();
+            
+            foreach (JProperty prop in docs.Properties())
+            {
+                SourceLinkEntry entry = new SourceLinkEntry();
+                entry.SymbolsPath = prop.Name;
+                entry.ServerPath = docs[prop.Name].Value<string>();
+                ret.Add(entry);
+            }
+
+            return new SourceLinkMap(ret.ToArray());
+        }
+    }
+}
