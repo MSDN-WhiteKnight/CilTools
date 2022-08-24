33c081a1bcf4d415ae09c2ba61095d14f90bca57
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2022-08-24 19:51:55 +0500

    Revert unneeded changes

--- CilTools.Metadata/Methods/MethodDef.cs
+++ CilTools.Metadata/Methods/MethodDef.cs
@@ -1,5 +1,5 @@
 ﻿/* CIL Tools 
- * Copyright (c) 2022,  MSDN.WhiteKnight (https://github.com/MSDN-WhiteKnight) 
+ * Copyright (c) 2021,  MSDN.WhiteKnight (https://github.com/MSDN-WhiteKnight) 
  * License: BSD 2.0 */
 using System;
 using System.Collections.Generic;
@@ -25,7 +25,7 @@ namespace CilTools.Metadata.Methods
             this.assembly = owner;
             this.mdef = m;
             this.mdefh = mh;
-            
+
             int rva = mdef.RelativeVirtualAddress;
 
             if (rva == 0)
