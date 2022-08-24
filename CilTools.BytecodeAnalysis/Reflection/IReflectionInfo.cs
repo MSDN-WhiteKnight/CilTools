9879ebfdef0e9a130798a7fd852f12352f582e36
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2022-08-24 19:59:42 +0500

    Add IReflectionInfo (#123)
    - Add IReflectionInfo
    - Add file properties in CIL View

---
+++ CilTools.BytecodeAnalysis/Reflection/IReflectionInfo.cs
@@ -0,0 +1,47 @@
+﻿/* CilTools.BytecodeAnalysis library 
+ * Copyright (c) 2022,  MSDN.WhiteKnight (https://github.com/MSDN-WhiteKnight) 
+ * License: BSD 2.0 */
+using System;
+
+namespace CilTools.Reflection
+{
+    /// <summary>
+    /// Provides additional information about reflection objects
+    /// </summary>
+    public interface IReflectionInfo
+    {
+        /// <summary>
+        /// Gets the value of the reflection property with the specified Id
+        /// </summary>
+        /// <param name="id">Property Id to get</param>
+        /// <returns>
+        /// The property value, or null if property is not supported
+        /// </returns>
+        object GetReflectionProperty(int id);
+    }
+
+    /// <summary>
+    /// Provides constants for reflection properties used with <see cref="IReflectionInfo"/>
+    /// </summary>
+    public static class ReflectionInfoProperties
+    {
+        /// <summary>
+        /// Represents the string containing information about object
+        /// </summary>
+        public const int InfoText = 1;
+
+        /// <summary>
+        /// Gets the value of the reflection property with the specified Id from the specified object
+        /// </summary>
+        /// <param name="obj">Reflection object to get property</param>
+        /// <param name="id">Property Id</param>
+        /// <returns>The property value, or null if property is not supported</returns>
+        public static object GetProperty(object obj, int id)
+        {
+            IReflectionInfo info = obj as IReflectionInfo;
+
+            if (info == null) return null;
+            else return info.GetReflectionProperty(id);
+        }
+    }
+}
---
+++ CilView.Core/Reflection/AssemblyInfoProvider.cs
@@ -0,0 +1,68 @@
+﻿/* CIL Tools 
+ * Copyright (c) 2022,  MSDN.WhiteKnight (https://github.com/MSDN-WhiteKnight) 
+ * License: BSD 2.0 */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using CilTools.Reflection;
+
+namespace CilView.Core.Reflection
+{
+    public static class AssemblyInfoProvider
+    {
+        static void ArrayBytesToText(byte[] arr, TextWriter target)
+        {
+            if (arr == null) return;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                target.Write(arr[i].ToString("X"));
+                target.Write(' ');
+            }
+
+            target.Flush();
+        }
+
+        public static string GetAssemblyInfo(Assembly ass)
+        {
+            StringBuilder sb = new StringBuilder(1000);
+            StringWriter wr = new StringWriter(sb);
+            AssemblyName an = ass.GetName();
+            wr.WriteLine("Name: " + an.Name);
+
+            if (an.Version != null)
+            {
+                wr.WriteLine("Version: " + an.Version.ToString());
+            }
+            
+            if (!string.IsNullOrEmpty(an.CultureName))
+            {
+                wr.WriteLine("Culture: " + an.CultureName);
+            }
+
+            byte[] token = an.GetPublicKeyToken();
+
+            if (token != null)
+            {
+                wr.Write("Public key token: ");
+                ArrayBytesToText(token, wr);
+                wr.WriteLine();
+            }
+
+            wr.WriteLine("Location: " + ass.Location);
+            wr.WriteLine();
+
+            string peinfo = ReflectionInfoProperties.GetProperty(ass, ReflectionInfoProperties.InfoText) as string;
+
+            if (peinfo != null)
+            {
+                wr.WriteLine(peinfo);
+            }
+
+            wr.Flush();
+            return sb.ToString();
+        }
+    }
+}
