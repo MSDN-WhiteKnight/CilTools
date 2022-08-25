dc87b2edc36fd85d1c30a2f42cde811d68358030
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2022-08-25 13:35:59 +0500

    Add tests for assembly properties

---
+++ tests/CilTools.Metadata.Tests/MetadataAssemblyTests.cs
@@ -0,0 +1,52 @@
+﻿/* CilTools.Metadata tests
+ * Copyright (c) 2022,  MSDN.WhiteKnight (https://github.com/MSDN-WhiteKnight) 
+ * License: BSD 2.0 */
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CilTools.Reflection;
+using CilTools.Tests.Common;
+using CilTools.Tests.Common.Attributes;
+using CilTools.Tests.Common.TextUtils;
+
+namespace CilTools.Metadata.Tests
+{
+    [TestClass]
+    public class MetadataAssemblyTests
+    {
+        [TestMethod]
+        public void Test_MetadataAssembly_InfoText()
+        {
+            AssemblyReader reader = ReaderFactory.GetReader();
+            Assembly ass = reader.LoadFrom(typeof(SampleMethods).Assembly.Location);
+            string info = (string)ReflectionInfoProperties.GetProperty(ass, ReflectionInfoProperties.InfoText);
+
+            Assert.IsFalse(string.IsNullOrEmpty(info));
+            Assert.IsTrue(info.Contains("Type: DLL"));
+            Assert.IsTrue(info.Contains("Subsystem: WindowsCui"));
+            Assert.IsTrue(info.Contains("File alignment: 512"));
+            Assert.IsTrue(info.Contains("Section alignment: 8192"));
+            Assert.IsTrue(info.Contains("OS version: 4.0"));
+            Assert.IsTrue(info.Contains("Machine type: I386"));
+            Assert.IsTrue(info.Contains("Characteristics: 0x2022 (ExecutableImage; LargeAddressAware; Dll; )"));
+            Assert.IsTrue(info.Contains("CorFlags: 0x1 (ILOnly; )"));
+        }
+
+        [ConditionalTest(TestCondition.NetFrameworkOnly, "Depends on strong names")]
+        public void Test_MetadataAssembly_InfoText_StrongName()
+        {
+            AssemblyReader reader = ReaderFactory.GetReader();
+            Assembly ass = reader.LoadFrom(typeof(object).Assembly.Location);
+            string info = (string)ReflectionInfoProperties.GetProperty(ass, ReflectionInfoProperties.InfoText);
+
+            Assert.IsFalse(string.IsNullOrEmpty(info));
+            Assert.IsTrue(info.Contains("Type: DLL"));
+            Assert.IsTrue(info.Contains("Subsystem: WindowsCui"));
+            AssertThat.IsMatch(info, new Text[] {
+                Text.Any, "CorFlags: ", Text.Any, "StrongNameSigned;", Text.Any 
+            });
+        }
+    }
+}
---
+++ tests/CilView.Tests/AssemblyInfoProviderTests.cs
@@ -0,0 +1,64 @@
+﻿/* CIL Tools
+ * Copyright (c) 2022,  MSDN.WhiteKnight (https://github.com/MSDN-WhiteKnight) 
+ * License: BSD 2.0 */
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CilTools.Metadata;
+using CilTools.Tests.Common;
+using CilView.Core.Reflection;
+
+namespace CilView.Tests
+{
+    [TestClass]
+    public class AssemblyInfoProviderTests
+    {
+        [TestMethod]
+        public void Test_GetAssemblyInfo_RuntimeReflection()
+        {
+            string info = AssemblyInfoProvider.GetAssemblyInfo(typeof(SampleMethods).Assembly);
+
+            Assert.IsFalse(string.IsNullOrEmpty(info));
+            Assert.IsTrue(info.Contains("Name: CilTools.Tests.Common"));
+            Assert.IsTrue(info.Contains("Version: "+ typeof(SampleMethods).Assembly.GetName().Version.ToString()));
+            Assert.IsTrue(info.Contains("Location: " + typeof(SampleMethods).Assembly.Location));
+        }
+
+        [TestMethod]
+        public void Test_GetAssemblyInfo()
+        {
+            AssemblyReader reader = new AssemblyReader();
+
+            using (reader)
+            {
+                Assembly ass = reader.LoadFrom(typeof(SampleMethods).Assembly.Location);
+                string info = AssemblyInfoProvider.GetAssemblyInfo(ass).Replace("\r\n", "\n");
+
+                Assert.IsFalse(string.IsNullOrEmpty(info));
+                Assert.IsTrue(info.Contains("Name: CilTools.Tests.Common"));
+                Assert.IsTrue(info.Contains("Version: " + typeof(SampleMethods).Assembly.GetName().Version.ToString()));
+                Assert.IsTrue(info.Contains("Location: " + typeof(SampleMethods).Assembly.Location));
+                Assert.IsTrue(info.Contains("Type: DLL"));
+                Assert.IsTrue(info.Contains("Subsystem: WindowsCui"));
+                Assert.IsTrue(info.Contains("CorFlags: 0x1 (ILOnly; )"));
+
+                Assert.IsTrue(info.Contains("Assembly custom attributes"));
+                Assert.IsTrue(info.Contains("TargetFrameworkAttribute"));
+
+                Assert.IsTrue(info.Contains(
+                    "AssemblyCompanyAttribute\n01 00 09 43 49 4C 20 54 6F 6F 6C 73 00 00 \n(...CIL Tools..)\n"
+                    ));
+
+                Assert.IsTrue(info.Contains(
+                    "AssemblyProductAttribute\n01 00 09 43 49 4C 20 54 6F 6F 6C 73 00 00 \n(...CIL Tools..)\n"
+                    ));
+
+                Assert.IsTrue(info.Contains("AssemblyTitleAttribute\n"+
+                    "01 00 15 43 69 6C 54 6F 6F 6C 73 2E 54 65 73 74 73 2E 43 6F 6D 6D 6F 6E 00 00 \n"+
+                    "(...CilTools.Tests.Common..)\n"));
+            }
+        }
+    }
+}
