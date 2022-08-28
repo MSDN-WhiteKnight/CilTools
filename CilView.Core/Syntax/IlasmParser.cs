1e800a39685f083ea2970ec25c291c538d5f61b6
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2022-08-28 13:54:04 +0500

    Add IlasmParser initial implementation
    Prep for displaying parts of .il files

---
+++ CilView.Core/Syntax/IlasmParser.cs
@@ -0,0 +1,102 @@
+﻿/* CIL Tools 
+ * Copyright (c) 2022,  MSDN.WhiteKnight (https://github.com/MSDN-WhiteKnight) 
+ * License: BSD 2.0 */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using CilTools.Syntax;
+using CilView.Common;
+
+namespace CilView.Core.Syntax
+{
+    public static class IlasmParser
+    {
+        /// <summary>
+        /// Transforms tokens sequence into an initial syntax tree shape, where every block ({...}) is 
+        /// represented as a subnode
+        /// </summary>
+        public static SyntaxNode TokensToInitialTree(IEnumerable<SyntaxNode> tokens)
+        {
+            SyntaxSequence root=new SyntaxSequence();
+            List<SyntaxSequence> currentPath = new List<SyntaxSequence>(10);
+            SyntaxSequence currNode = root;
+            SyntaxSequence newNode;
+            
+            foreach (SyntaxNode token in tokens)
+            {
+                if (token is PunctuationSyntax)
+                {
+                    if (Utils.StringEquals(((PunctuationSyntax)token).Content, "}"))
+                    {
+                        if (currentPath.Count == 0)
+                        {
+                            throw new Exception("Unexpected closing brace");
+                        }
+
+                        newNode = currentPath[currentPath.Count - 1];
+                        currentPath.RemoveAt(currentPath.Count - 1);
+
+                        if (currentPath.Count > 0) currNode = currentPath[currentPath.Count - 1];
+                        else currNode = root;
+
+                        newNode.Add(token);
+                        currNode.Add(newNode);
+                    }
+                    else if (Utils.StringEquals(((PunctuationSyntax)token).Content, "{"))
+                    {
+                        newNode = new SyntaxSequence();
+                        newNode.Add(token);
+                        currentPath.Add(newNode);
+                        currNode = newNode;
+                    }
+                    else
+                    {
+                        currNode.Add(token);
+                    }
+                }
+                else
+                {
+                    currNode.Add(token);
+                }
+            }
+
+            return root;
+        }
+
+        class SyntaxSequence : SyntaxNode
+        {
+            List<SyntaxNode> _children;
+
+            public SyntaxSequence(IEnumerable<SyntaxNode> children)
+            {
+                this._children = new List<SyntaxNode>(children);
+            }
+
+            public SyntaxSequence()
+            {
+                this._children = new List<SyntaxNode>(100);
+            }
+
+            public void Add(SyntaxNode node)
+            {
+                this._children.Add(node);
+            }
+
+            public override IEnumerable<SyntaxNode> EnumerateChildNodes()
+            {
+                return this._children.ToArray();
+            }
+
+            public override void ToText(TextWriter target)
+            {
+                for (int i = 0; i < _children.Count; i++)
+                {
+                    _children[i].ToText(target);
+                }
+
+                target.Flush();
+            }
+        }
+    }
+}
---
+++ tests/CilView.Tests/IlAsmParserTests.cs
@@ -0,0 +1,42 @@
+﻿/* CIL Tools
+ * Copyright (c) 2022,  MSDN.WhiteKnight (https://github.com/MSDN-WhiteKnight) 
+ * License: BSD 2.0 */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CilTools.Syntax;
+using CilTools.Tests.Common;
+using CilView.Core.Syntax;
+
+namespace CilView.Tests
+{
+    [TestClass]
+    public class IlAsmParserTests
+    {
+        [TestMethod]
+        public void Test_TokensToInitialTree_Simple()
+        {
+            SyntaxNode[] tokens = new SyntaxNode[] {
+                SyntaxFactory.CreateFromToken(".class", string.Empty, " "),
+                SyntaxFactory.CreateFromToken("private", string.Empty, " "),
+                SyntaxFactory.CreateFromToken("Foo", string.Empty, " "),
+                SyntaxFactory.CreateFromToken("{", string.Empty, " "),
+                SyntaxFactory.CreateFromToken(".field", string.Empty, " "),
+                SyntaxFactory.CreateFromToken("private", string.Empty, " "),
+                SyntaxFactory.CreateFromToken("int32", string.Empty, " "),
+                SyntaxFactory.CreateFromToken("Bar", string.Empty, " "),
+                SyntaxFactory.CreateFromToken("}", string.Empty, " "),
+            };
+
+            SyntaxNode tree = IlasmParser.TokensToInitialTree(tokens);
+
+            SyntaxNode[] items = tree.GetChildNodes();
+            Assert.AreEqual(4, items.Length);
+            Assert.AreEqual(".class ", items[0].ToString());
+            Assert.AreEqual("private ", items[1].ToString());
+            Assert.AreEqual("Foo ", items[2].ToString());
+            Assert.AreEqual("{ .field private int32 Bar } ", items[3].ToString());            
+        }
+    }
+}
