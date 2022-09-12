f785be58b213c0c43d067337ae8ce257af522bf2
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2022-09-12 09:50:04 +0500

    Update ViewCommand.cs

--- CilTools.CommandLine/ViewCommand.cs
+++ CilTools.CommandLine/ViewCommand.cs
@@ -29,9 +29,9 @@ namespace CilTools.CommandLine
             {
                 string exeName = typeof(Program).Assembly.GetName().Name;
 
-                yield return TextParagraph.Text("Print disassembled CIL code of the specified type or method:");
+                yield return TextParagraph.Text("Print disassembled CIL code of the specified assembly, type or method:");
                 yield return TextParagraph.Code("    " + exeName +
-                    " view [--nocolor] <assembly path> <type full name> [<method name>]");
+                    " view [--nocolor] <assembly path> [<type full name>] [<method name>]");
                 yield return TextParagraph.Text("Print contents of the specified CIL source file (*.il):");
                 yield return TextParagraph.Code("    " + exeName + " view [--nocolor] <source file path>");
                 yield return TextParagraph.Text(string.Empty);
@@ -89,7 +89,7 @@ namespace CilTools.CommandLine
             }
 
             if (FileUtils.HasCilSourceExtension(filepath) ||
-                (args.Length < 4 && !FileUtils.HasPeFileExtension(filepath)))
+                (args.Length < 3 && !FileUtils.HasPeFileExtension(filepath)))
             {
                 //view IL source file
 
@@ -110,20 +110,20 @@ namespace CilTools.CommandLine
                 }
             }
 
+            Console.WriteLine("Assembly: " + filepath);
+            
             //read type and method name from arguments
             type = CLI.ReadCommandParameter(args, pos);
             pos++;
 
             if (type == null)
             {
-                Console.WriteLine("Error: Type name is not provided for the 'view' command.");
-                Console.WriteLine(CLI.GetErrorInfo());
-                return 1;
+                //view assembly manifest
+                Console.WriteLine();
+                return Disassembler.DisassembleAssembly(filepath, noColor, Console.Out);
             }
 
             method = CLI.ReadCommandParameter(args, pos);
-
-            Console.WriteLine("Assembly: " + filepath);
 
             if (string.IsNullOrEmpty(method))
             {
