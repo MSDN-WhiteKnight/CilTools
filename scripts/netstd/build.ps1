5fede7d2970cfddc79a62c5578cee6533b0aa9c4
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2022-09-16 09:10:38 +0500

    Fix scripts

--- scripts/netstd/build.ps1
+++ scripts/netstd/build.ps1
@@ -2,7 +2,7 @@ Param($config = "Debug")
 
 echo ('Configuration: '+$config)
 
-$toBuild = "CilTools.Metadata/CilTools.Metadata.csproj -f netstandard2.0",
+$toBuild = "tests/CilTools.Metadata.Tests/CilTools.Metadata.Tests.csproj -f netcoreapp3.1",
            "tests/CilTools.BytecodeAnalysis.Tests/CilTools.BytecodeAnalysis.Tests.csproj -f netcoreapp3.1",
            "tests/CilTools.CommandLine.Tests/CilTools.CommandLine.Tests.csproj",
            "tests/CilView.Tests/CilView.Tests.csproj"
--- scripts/netstd/test.ps1
+++ scripts/netstd/test.ps1
@@ -3,6 +3,7 @@ Param($config = "Debug")
 echo ('Configuration: '+$config)
 
 $toTest = "tests/CilTools.BytecodeAnalysis.Tests/CilTools.BytecodeAnalysis.Tests.csproj -f netcoreapp3.1",
+          "tests/CilTools.Metadata.Tests/CilTools.Metadata.Tests.csproj -f netcoreapp3.1",
           "tests/CilTools.CommandLine.Tests/CilTools.CommandLine.Tests.csproj",
           "tests/CilView.Tests/CilView.Tests.csproj"
 
