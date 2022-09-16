6610a10d2a9523e4c125966e0c1d348909338945
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2022-09-16 09:06:27 +0500

    Update .NET Standard scripts

--- scripts/netstd/test.ps1
+++ scripts/netstd/test.ps1
@@ -1,5 +1,8 @@
+Param($config = "Debug")
+
+echo ('Configuration: '+$config)
+
 $toTest = "tests/CilTools.BytecodeAnalysis.Tests/CilTools.BytecodeAnalysis.Tests.csproj -f netcoreapp3.1",
-          "tests/CilTools.Metadata.Tests/CilTools.Metadata.Tests.csproj -f netcoreapp3.1",
           "tests/CilTools.CommandLine.Tests/CilTools.CommandLine.Tests.csproj",
           "tests/CilView.Tests/CilView.Tests.csproj"
 
@@ -8,8 +11,9 @@ $exitCode = 0
 foreach ($item in $toTest)
 {
     echo ("Testing "+$item)
-    Invoke-Expression ("dotnet test "+$item)
+    Invoke-Expression ("dotnet test "+$item+" -c "+$config)
     if ($LASTEXITCODE -ne 0) {$exitCode = $LASTEXITCODE}
+    echo ""
 }
 
 echo "Finished testing"
